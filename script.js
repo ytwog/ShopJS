17cded3a559972abcd96f20dbef5b29ccd7c4346
Vyacheslav <ytwog@mail.ru>
2020-08-19 22:54:09 +0700

    basic transform json file into html

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
+const _DEBUGGING = false;
+const _RACE = "Zerg";
+
+function getElements(node) {
+    let itemCount = node.list.length;
+    let mainDiv = $('<div class="col-4 text-light bg-dark m-1 col-md-2"></div>');
+    node.list.forEach((element) => {
+        let elementImg = $(`<img src="${element.thumbnail}" alt="${element.alt}" class="item-boxover">`)
+        let elementName = $(`<div>${element.name}</div>`);
+        mainDiv.append(elementImg);
+        mainDiv.append(elementName);
+    });
+    return mainDiv;
+}
+
 function updateShop() {
     $.getJSON( "json/shopData.json", function( data ) {
-      console.log(data);
+        if(_DEBUGGING) {
+          console.log(data);
+        }
+        let menuBuy = $('#menu-buy');
+        data.list.forEach((element)=>{
+            if(_RACE == element["race"])
+                menuBuy.append(getElements(element));
+        })
     });
 }
 updateShop();

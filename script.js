15b7b96f7058d8aebbdc13bc1473522a146aa0a8
Vyacheslav <ytwog@mail.ru>
2020-08-20 21:39:22 +0700

    Element placement improvements

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
 const _DEBUGGING = false;
 const _RACE = "Zerg";
 
-function getElements(node) {
+function insertElementsFromJSON(node, placeholderID1, placeholderID2) {
     let itemCount = node.list.length;
-    let mainDiv = $('<div class="col-4 text-light bg-dark m-1 col-md-2"></div>');
+    let buildingDiv = $('<div class="row">');
+    let unitsDiv = $('<div class="row">');
+    $(placeholderID1).append(unitsDiv);
+    $(placeholderID2).append(buildingDiv);
     node.list.forEach((element) => {
-        let elementImg = $(`<img src="${element.thumbnail}" alt="${element.alt}" class="item-boxover">`)
-        let elementName = $(`<div>${element.name}</div>`);
-        mainDiv.append(elementImg);
+        let mainDiv = $('<div class="col-12 col-md-4 col-lg-3 text-light bg-dark m-1"></div>');
+        let elementImgWrapper = $('<div class="img-wrapper d-none d-lg-block d-md-block d-xl-block"></div>');
+        let elementImg = $(`<img src="${element.thumbnail}" alt="${element.alt}" class="item-boxover m-75">`)
+        let elementName = $(`<div><p>${element.name}</p></div>`);
+        elementImgWrapper.append(elementImg);
+        mainDiv.append(elementImgWrapper);
         mainDiv.append(elementName);
+        console.log(element.isBuilding);
+        if(element.isBuilding) {
+            buildingDiv.append(mainDiv);
+        } else {
+            unitsDiv.append(mainDiv);
+        }
     });
-    return mainDiv;
 }
 
 function updateShop() {
@@ -18,10 +29,9 @@ function updateShop() {
         if(_DEBUGGING) {
           console.log(data);
         }
-        let menuBuy = $('#menu-buy');
         data.list.forEach((element)=>{
             if(_RACE == element["race"])
-                menuBuy.append(getElements(element));
+                insertElementsFromJSON(element, '#menu-buy-units', '#menu-buy-buildings');
         })
     });
 }

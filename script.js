3299ffb102c69d533a858ff3121d1286d23e9e03
Vyacheslav <ytwog@mail.ru>
2020-08-25 14:18:08 +0700

    Interface/Fuctional update. +Minor transitions, +Resources info

--- script.js
+++ script.js
@@ -3,23 +3,34 @@ const _RACE = "Zerg";
 
 function insertElementsFromJSON(node, placeholderID1, placeholderID2) {
     let itemCount = node.list.length;
-    let buildingDiv = $('<div class="row mt-2">');
-    let unitsDiv = $('<div class="row mt-2 p-sm-0">');
+    let buildingDiv = $('<div class="row">');
+    let unitsDiv = $('<div class="row">');
     $(placeholderID1).append(unitsDiv);
     $(placeholderID2).append(buildingDiv);
-    node.list.forEach((element) => {
-        let mainDiv = $('<div class="col-12 p-1 p-md-2 col-md-auto text-light bg-dark m-1 mr-0 "></div>');
-        let elementImgWrapper = $('<div class="img-wrapper mb-md-2 m-0"></div>');
+    node.list.forEach((element, index) => {
+        //let sizeLg = index % 
+        let mainWrapper = $('<div class="col-12 p-1 p-md-2 col-md-6 col-xl-4 text-light bg-secondary transition-width"></div>');
+        let mainDiv = $('<div class="bg-dark h-100"></div>');
+        let elementImgWrapper = $('<div class="img-wrapper mb-md-2"></div>');
         let elementImg = $(`<img src="${element.thumbnail}" alt="${element.alt}" class="item-boxover">`)
-        let elementName = $(`<div class="d-none d-lg-block d-md-block d-xl-block"><p>${element.name}</p></div>`);
+        let elementName = $(`<div class="d-none d-lg-block py-1 bg-black d-xl-block"><p class=" p-0 m-0">${element.name}</p></div>`);
+        let elementCost = $(`<div class="d-none d-sm-block py-1 bg-black d-md-block d-lg-block d-xl-block">
+                                    <div class="img-wrapper-sm mb-md-2 m-0 text-white">
+                                        <img src="images/gGlass.png" alt="Minerals" class="item-boxover"> 
+                                        <div class="item-info">
+                                                 X${element.costX}
+                                        </div>
+                                    </div>
+                            </div>`);
         elementImgWrapper.append(elementImg);
+        mainWrapper.append(mainDiv);
         mainDiv.append(elementImgWrapper);
         mainDiv.append(elementName);
-        console.log(element.isBuilding);
+        mainDiv.append(elementCost);
         if(element.isBuilding) {
-            buildingDiv.append(mainDiv);
+            buildingDiv.append(mainWrapper);
         } else {
-            unitsDiv.append(mainDiv);
+            unitsDiv.append(mainWrapper);
         }
     });
 }

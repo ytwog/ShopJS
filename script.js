2c1cd9e86c57bb6b398aefbccc21ab52cb0a3e78
Vyacheslav <ytwog@mail.ru>
2020-08-27 23:51:19 +0700

    Interface fixes/Resoures displays in correct way/Task order basic implementation

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const _DEBUGGING = false;
 const _RACE = "Zerg";
 
+function deliver(event) {
+    let package = $(`<div>${event.data.paramData}</div>`)
+    $('#task-space').append(package);
+}
+
 function insertElementsFromJSON(node, placeholderID1, placeholderID2) {
     let itemCount = node.list.length;
     let buildingDiv = $('<div class="row">');
@@ -9,18 +14,20 @@ function insertElementsFromJSON(node, placeholderID1, placeholderID2) {
     $(placeholderID2).append(buildingDiv);
     node.list.forEach((element, index) => {
         //let sizeLg = index % 
-        let mainWrapper = $('<div class="col-12 p-1 p-md-2 col-md-6 col-xl-4 text-light bg-secondary transition-width"></div>');
-        let mainDiv = $('<div class="bg-dark h-100"></div>');
+        let mainWrapper = $('<div class="col-12 p-1 p-md-2 col-md-6 col-xl-4 text-light bg-secondary"></div>');
+        let mainDiv = $('<div class="item-wrap bg-dark h-100"></div>');
         let elementImgWrapper = $('<div class="img-wrapper mb-md-2"></div>');
         let elementImg = $(`<img src="${element.thumbnail}" alt="${element.alt}" class="item-boxover">`)
-        let elementName = $(`<div class="d-none d-lg-block py-1 bg-black d-xl-block"><p class=" p-0 m-0">${element.name}</p></div>`);
-        let elementCost = $(`<div class="d-none d-sm-block py-1 bg-black d-md-block d-lg-block d-xl-block">
-                                    <div class="img-wrapper-sm mb-md-2 m-0 text-white">
-                                        <img src="images/gGlass.png" alt="Minerals" class="item-boxover"> 
-                                        <div class="item-info">
-                                                 X${element.costX}
-                                        </div>
-                                    </div>
+        let elementName = $(`<div class="d-none d-sm-block d-lg-block py-1 bg-black d-xl-block"><p class=" p-0 m-0">${element.name}</p></div>`);
+        let elementCost = $(`<div class="d-none d-sm-block py-1 pl-1 bg-black d-md-block d-lg-block d-xl-block">
+                                <div class="inlined">
+                                    <img src="images/gGlass.png" alt="Minerals" class="inlined img-sm"> 
+                                    <div class="inlined item-info">X${element.costX}</div>
+                                </div>
+                                <div class="inlined">
+                                    <img src="images/gGas.png" alt="Minerals" class="inlined img-sm"> 
+                                    <div class="inlined item-info">X${element.costY}</div>
+                                </div>
                             </div>`);
         elementImgWrapper.append(elementImg);
         mainWrapper.append(mainDiv);
@@ -32,6 +39,7 @@ function insertElementsFromJSON(node, placeholderID1, placeholderID2) {
         } else {
             unitsDiv.append(mainWrapper);
         }
+        mainDiv.click({paramData: element.name}, deliver);
     });
 }
 
